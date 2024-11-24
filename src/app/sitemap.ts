fe68d79f556640d6a1ef180368c4c22045fc81b1
Harry Northover <harry@nrthpoint.com>
2024-11-24 21:07:33 +0000

    Add artlcles list

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,6 +5,7 @@ import { getArticles } from "@/lib/api";
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const articles = await getArticles();
   const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://nrthpoint.com";
+  const articleListUrl = `${siteUrl}/articles`;
 
   const workUrls = articles.map((article) => ({
     url: `${siteUrl}/work/${article.url}`,
@@ -13,6 +14,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
   return [
     { url: siteUrl, lastModified: new Date().toISOString() },
+    { url: articleListUrl, lastModified: new Date().toISOString() },
     ...workUrls,
   ];
 }
