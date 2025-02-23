87a21673fac6c9ecd8f1e1e794ac542a5bce75ef
Harry Northover <harry@nrthpoint.com>
2025-02-23 17:48:36 +0000

    Width fix

--- src/components/LinkItem.tsx
+++ src/components/LinkItem.tsx
@@ -11,17 +11,15 @@ export const LinkItem: React.FC<LinkItemProps> = ({
   name,
   description,
 }) => (
-  <div className="min-w-[200px]">
-    <div>
-      <Link
-        href={url || ""}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="flex mb-1"
-      >
-        <h3 className="pb-0">{name}</h3>
-      </Link>
-      <span className="block leading-7">{description}</span>
-    </div>
+  <div>
+    <Link
+      href={url || ""}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="flex mb-1"
+    >
+      <h3 className="pb-0">{name}</h3>
+    </Link>
+    <span className="block leading-7">{description}</span>
   </div>
 );
