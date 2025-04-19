6c4c1101d1d614a99a896a91db5017fa42cdbf8d
Harry Northover <a-harry.northover@rvu.co.uk>
2025-04-19 17:15:29 +0100

    Husky & lint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
+import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Inter, Lato, Merriweather } from "next/font/google";
 import { draftMode } from "next/headers";
-import { Analytics } from "@vercel/analytics/react";
 
 import "./globals.css";
 
